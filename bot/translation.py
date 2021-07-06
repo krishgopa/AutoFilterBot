d9595750cbdbcabc1965a5a01162e375f27fba9c
krishgopa <87017521+krishgopa@users.noreply.github.com>
2021-07-06 17:56:57 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,18 +5,15 @@
 class Translation(object):
     
     START_TEXT = """<b><u>Hey {} !! </u>☺️
-<i>ᴀᴍ ᴊᴜsᴛ ᴀ ᴀᴅᴠᴀɴᴄᴇ ᴀᴜᴛᴏ ғɪʟᴛᴇʀ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ᴛᴏ ᴡᴏʀᴋ ɪɴ <i>𝕄𝕒𝕝𝕒𝕪𝕒𝕝𝕒𝕞 ℝ𝕠𝕔𝕜𝕖𝕣𝕤 𝔾𝕣𝕠𝕦𝕡. 😉.</i>
+<i>ᴀᴍ ᴊᴜsᴛ ᴀ ᴀᴅᴠᴀɴᴄᴇ ᴀᴜᴛᴏ ғɪʟᴛᴇʀ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ᴛᴏ ᴡᴏʀᴋ . 😉.</i>
 𝙔𝙤𝙪 𝘾𝙖𝙣'𝙩 𝘼𝙙𝙙 𝙈𝙚 𝙩𝙤 𝙔𝙤𝙪𝙧 𝙂𝙧𝙤𝙪𝙥!! 🍒
 
-You can create Your own by Clicking on Deploy Tutorial !!!😎 </b>
+You can create Your own  !!!😎 </b>
 
  
 </i>"""    
     
-    HELP_TEXT = """
-<b>Create Your Auto Filter Using This Link \n:- https://youtu.be/qW9o9G7stCk ☺️☺️ </b>
-
-"""
+    
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
@@ -26,5 +23,5 @@ You can create Your own by Clicking on Deploy Tutorial !!!😎 </b>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Deploy Your Own</b> : <i><a href="https://youtu.be/qW9o9G7stCk">Click Me</a></i>
+
 """
